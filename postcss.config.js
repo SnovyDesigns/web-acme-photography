6dc09a3eef66b749bfb9d9c921537210f95d3b59
Krzysztof Piekarz <krzysztofpiekarz@iMac-Krzysztof.local>
2018-08-25 14:13:58 +0200

    Add styles and media-queries

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  plugins: [require('autoprefixer'), require('css-mqpacker')]
+  plugins: [
+    require('autoprefixer'),
+    require('postcss-combine-duplicated-selectors'),
+    require('css-mqpacker')
+  ]
 };
